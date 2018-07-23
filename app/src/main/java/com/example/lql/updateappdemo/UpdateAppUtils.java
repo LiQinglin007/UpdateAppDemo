0915258cc57a93dfc0991824e6aa902b357dd4d2
hanzhi_dell <531192557@qq.com>
2018-07-23 12:36:33 +0800

    兼容8.0

--- app/src/main/java/com/example/lql/updateappdemo/UpdateAppUtils.java
+++ app/src/main/java/com/example/lql/updateappdemo/UpdateAppUtils.java
@@ -13,6 +13,7 @@ import android.os.Handler;
 import android.os.Message;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 
 import android.support.v4.app.Fragment;
@@ -31,37 +32,70 @@ import java.lang.ref.WeakReference;
 
 
 /**
- * 类描述：版本更新工具类
- * 这里使用DownLoadManager来做下载，在UpdateAppService类中
- * 在UpdateAppUtils检测是否要下载
- * 逻辑关系说明：
- * 1、请求数据  拿到后台的apk的版本号、版本名、更新内容、下载地址（可能还会包括是否强制更新的标志位）
- * 2、和本地的apk版本号对比时候需要更新
- * 3、这里用户可能会忽略此版本，这时候我要存储一个版本号，用户忽略的版本号，如果用户的是主动更新的，都要提示。如果不是主动更新的判断一下，判断一下。
- * 4、在步骤2以后   如果需要更新，先判断步骤3里边存储的版本号，比较大小，
- * 如果用户忽略更新这个版本，那就不更新了；如果用户没有忽略过此版本，继续往下
- * 5、这时候给用户一个提示，展示内容是有新版本是否更新，更新内容
- * 5.1  这里可能涉及到一个强制更新，如果是强制更新的，那就要求，用户没有更新，就退出程序，不是强制更新的，可以忽略此版本
- * 6、启动下载服务
- * 7、下载完成之后的自动安装
- * 使用说明：
- * 在外部直接调用UpdateApp()方法，并且需要重写onActivityResult()方法和onRequestPermissionsResult()方法
- * 作  者：李清林
- * 时  间：2018.5.2
- * 修改备注：增加申请权限功能
+ * 类描述：版本更新工具类<br>
+ * 这里使用DownLoadManager来做下载，在UpdateAppService类中<br>
+ * 在UpdateAppUtils检测是否要下载<br>
+ * 逻辑关系说明：<br>
+ * 1、请求数据  拿到后台的apk的版本号、版本名、更新内容、下载地址（可能还会包括是否强制更新的标志位）<br>
+ * 这里有一点需要注意，如果有强制更新的概念，这时候请求数据的时候要给服务器你当前的版本号，<br>
+ * 如果你现在的版本号是1  ，用户很久没有操作，期间版本更新了2（强制更新）、3（不强制更新）。这时候用户打开版本号为1的版本，应该是要强制更新的。<br>
+ * 2、和本地的apk版本号对比时候需要更新<br>
+ * 3、这里用户可能会忽略此版本，这时候我要存储一个版本号，用户忽略的版本号，如果用户的是主动更新的，都要提示。如果不是主动更新的判断一下，判断一下。<br>
+ * 4、在步骤2以后   如果需要更新，先判断步骤3里边存储的版本号，比较大小，<br>
+ * 如果用户忽略更新这个版本，那就不更新了；如果用户没有忽略过此版本，继续往下<br>
+ * 5、这时候给用户一个提示，展示内容是有新版本是否更新，更新内容<br>
+ * 5.1  这里可能涉及到一个强制更新，如果是强制更新的，那就要求，用户没有更新，就退出程序，不是强制更新的，可以忽略此版本<br>
+ * 6、启动下载服务<br>
+ * 7、下载完成之后的自动安装<br>
+ * 8、不要忘记兼容7.0  在res/xml文件中添加    file_paths.xml 文件，并且在AndroidManifest.xml文件中添加
+ * 使用说明：<br>
+ * 在外部直接调用UpdateApp()方法<br>
+ * 作  者：李清林<br>
+ * 时  间：2017.5.12<br>
+ * 更新时间：2018.5.2<br>
+ * 修改备注：增加申请权限功能<br>
+ * 更新时间：2018.7.23<br>
+ * 修改备注：兼容8.0<br>
  */
 public class UpdateAppUtils {
-    private static int mVersionCode = 0;//当前版本号
+    /**
+     * 当前版本号
+     */
+    private static int mVersionCode = 0;
     private static Activity mActivity;
     private static Fragment mFragment;
-    private static String mDownloadUrl = "";//下载地址
-    public static int REQUEST_PERMISSION_SDCARD_6_0 = 0x56;//写SD卡权限
-    public static int REQUEST_PERMISSION_SDCARD_SETTING = 0x57;//去设置页面
-    private static int serviceVersionCode = 0;//服务器的版本号码
-    private static String serviceVersionName = "";//服务器的版本号名称
-    private static boolean IsUpdate = false;//是否强制更新
-    private static String content = "";//更新说明
-
+    /**
+     * 下载地址
+     */
+    private static String mDownloadUrl = "";
+    /**
+     * 写SD卡权限
+     */
+    public static int REQUEST_PERMISSION_SDCARD_6_0 = 0x56;
+    /**
+     * 允许安装未知来源权限
+     */
+    public static int REQUEST_PERMISSION_SDCARD_8_0 = 0x58;
+    /**
+     * 去设置页面
+     */
+    public static int REQUEST_PERMISSION_SDCARD_SETTING = 0x57;
+    /**
+     * 服务器的版本号码
+     */
+    private static int serviceVersionCode = 0;
+    /**
+     * 服务器的版本号名称
+     */
+    private static String serviceVersionName = "";
+    /**
+     * 是否强制更新
+     */
+    private static boolean IsUpdate = false;
+    /**
+     * 更新说明
+     */
+    private static String content = "";
 
     public UpdateAppUtils() {
     }
@@ -95,8 +129,10 @@ public class UpdateAppUtils {
             e.printStackTrace();
         }
 
-        if (mVersionCode < serviceVersionCode) {//第2步骤
-            if (PreferenceUtils.getInt(FinalData.VERSIONCODE, 0) < serviceVersionCode || IsToast) {//第3步骤
+        if (mVersionCode < serviceVersionCode) {
+            //第2步骤
+            if (PreferenceUtils.getInt(FinalData.VERSIONCODE, 0) < serviceVersionCode || IsToast) {
+                //第3步骤
                 //这时候要去更新，展示下载的对话框
                 showDownLoadDialog();
             }
@@ -120,22 +156,10 @@ public class UpdateAppUtils {
                     @Override
                     public void onClick(DialogInterface arg0, int arg1) {//第6步骤，下载
                         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                            if (ContextCompat.checkSelfPermission(mActivity, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                                //有权限去下载
-                                downLoad();
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                checkPermission();
                             } else {
-                                // 该方法在用户上次拒绝后调用,因为已经拒绝了这次你还要申请授权你得给用户解释一波
-                                // 一般建议弹个对话框告诉用户 该方法在6.0之前的版本永远返回的是fasle
-                                if (ActivityCompat.shouldShowRequestPermissionRationale(mActivity, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-                                    getPermissionDialog();
-                                } else {
-                                    // 申请授权
-                                    if (mFragment != null)
-                                        mFragment.requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_PERMISSION_SDCARD_6_0);
-                                    else
-                                        ActivityCompat.requestPermissions(mActivity,
-                                                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_PERMISSION_SDCARD_6_0);
-                                }
+                                downLoad();
                             }
                         } else {
                             downLoad();
@@ -161,6 +185,46 @@ public class UpdateAppUtils {
     }
 
     /**
+     * 检查6.0权限
+     */
+    private static void checkPermission() {
+        if (ContextCompat.checkSelfPermission(mActivity, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                checkInstallPermission();
+            } else {
+                //有权限去下载
+                downLoad();
+            }
+        } else {
+            // 该方法在用户上次拒绝后调用,因为已经拒绝了这次你还要申请授权你得给用户解释一波
+            // 一般建议弹个对话框告诉用户 该方法在6.0之前的版本永远返回的是fasle
+            if (ActivityCompat.shouldShowRequestPermissionRationale(mActivity, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                getPermissionDialog(0);
+            } else {// 申请授权
+                if (mFragment != null) {
+                    mFragment.requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_PERMISSION_SDCARD_6_0);
+                } else {
+                    ActivityCompat.requestPermissions(mActivity,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_PERMISSION_SDCARD_6_0);
+                }
+            }
+        }
+    }
+
+    /**
+     * 检查8.0权限
+     */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private static void checkInstallPermission() {
+        boolean b = mActivity.getPackageManager().canRequestPackageInstalls();
+        if (b) {
+            downLoad();
+        } else {
+            getPermissionDialog(1);
+        }
+    }
+
+    /**
      * 正式去下载
      */
     private static void downLoad() {
@@ -181,22 +245,37 @@ public class UpdateAppUtils {
 
     /**
      * 申请权限的对话框
+     *
+     * @param type 0:6.0   1:8.0
      */
-    private static void getPermissionDialog() {
+    private static void getPermissionDialog(int type) {
+        String message = "更新软件需要您允许我们获取您的数据读写权限,否则将无法更新";
+        if (type == 1) {
+            message = "为了正常升级APP，请点击设置-高级设置-允许安装未知来源应用，本功能只限用于APP版本升级";
+        }
         AlertDialog dialog = new AlertDialog.Builder(mActivity).
                 setCancelable(false).
                 setTitle("权限提醒").
-                setMessage("更新软件需要您允许我们获取您的数据读写权限,否则将无法更新").
+                setMessage(message).
                 setPositiveButton("权限设置", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface arg0, int arg1) {//第6步骤，下载
+
                         Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
                         Uri uri = Uri.fromParts("package", mActivity.getPackageName(), null);
-                        intent.setData(uri);
-                        if (mFragment != null)
+
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                            //注意这个是8.0新API,直接跳转到允许安装位置来源的页面
+                            intent = new Intent(Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES, uri);
+                        } else {
+                            intent.setData(uri);
+                        }
+
+                        if (mFragment != null) {
                             mFragment.startActivityForResult(intent, REQUEST_PERMISSION_SDCARD_SETTING);
-                        else
+                        } else {
                             mActivity.startActivityForResult(intent, REQUEST_PERMISSION_SDCARD_SETTING);
+                        }
                     }
                 }).
                 setNegativeButton("取消", new DialogInterface.OnClickListener() {
@@ -230,10 +309,25 @@ public class UpdateAppUtils {
         mActivity = activity;
         mFragment = fragment;
         if (requestCode == REQUEST_PERMISSION_SDCARD_6_0) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    checkInstallPermission();
+                } else {
+                    //有权限去下载
+                    downLoad();
+                }
+            } else {
+                getPermissionDialog(0);
+            }
+        } else if (requestCode == REQUEST_PERMISSION_SDCARD_8_0) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 downLoad();
-            else
-                getPermissionDialog();
+            } else {
+                getPermissionDialog(1);
+            }
+        } else if (requestCode == REQUEST_PERMISSION_SDCARD_SETTING) {
+            //设置页面
+            checkPermission();
         }
     }
 
@@ -247,7 +341,7 @@ public class UpdateAppUtils {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
-            mWeakReference.get().exitApp();
+            exitApp();
         }
     }
 
